4118f70350a6062822f4a3924fbd8d08e74e974c
Mariusz <mariusz.c008@protonmail.com>
2023-09-27 20:47:30 +0200

    animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,20 @@ module.exports = {
   theme: {
     
     extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
       colors: {
         'wtf': '#557788'
       },
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
       }
     },
   },
